d0fa9c9940807bb95a5066d7fc118b75b9536e8c
buigabor <bui.gabor97@gmail.com>
2020-09-29 20:16:24 +0200

    Add 'add ingredient' option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ShoppingCart';
   currentPage = 'recipes';
-  showPage(page: string) {
+  showPage(page: string): void {
     this.currentPage = page;
   }
 }
