ca7265fed355c88afe0da619bd0b2fbf5a6d1036
buigabor <bui.gabor97@gmail.com>
2020-10-18 11:36:33 +0200

    Add Angular Universal, SSR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { AuthService } from './auth/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromAppReduce from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,13 @@ export class AppComponent implements OnInit {
   title = 'ShoppingCart';
   constructor(
     private authService: AuthService,
-    private store: Store<fromAppReduce.AppState>
+    private store: Store<fromAppReduce.AppState>,
+    @Inject(PLATFORM_ID) private platformId
   ) {}
 
   ngOnInit(): void {
-    this.store.dispatch(AuthActions.autoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(AuthActions.autoLogin());
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { RecipeEffects } from './recipes/store/recipe.effects';
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
