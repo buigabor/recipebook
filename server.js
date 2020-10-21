042455c0d2df40d54369320e7223c42c381d2f3a
buigabor <bui.gabor97@gmail.com>
2020-10-21 06:38:51 +0200

    Update server.js for heroku deployment

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ShoppingCart'));
+app.use(express.static(__dirname + '/dist/ShoppingCart/browser'));
 
 app.get('/*', (request,response) => {
-  response.sendFile(path.join(__dirname+'/dist/ShoppingCart/index.html'));
+  response.sendFile(path.join(__dirname+'/dist/ShoppingCart/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
